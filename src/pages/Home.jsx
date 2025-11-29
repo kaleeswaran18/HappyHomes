bbf9efbacc116ae774a161e8a426e79899a6542e
kaleeswaran18 <kaleesvk1997@gmail.com>
2025-11-29 10:22:47 +0530

    check

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -635,23 +635,7 @@ const [projects, setProjects] = useState([]);
               and designed with your comfort in mind.
             </p>
 
-            <div className="button-wrapper">
-              <a
-                href="https://jayabharath.com/media/"
-                className="more-button"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <svg
-                  viewBox="0 0 192 512"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="arrow-icon"
-                >
-                  <path d="M0 384.662V127.338c0-17.818 21.543-26.741 34.142-14.142l128.662 128.662c7.81 7.81 7.81 20.474 0 28.284L34.142 398.804C21.543 411.404 0 402.48 0 384.662z"></path>
-                </svg>
-                <span>More About Us</span>
-              </a>
-            </div>
+            
           </div>
 
           {/* Right Section (Empty for Background / Image use) */}
